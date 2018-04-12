01a6b5cfec361d91076dece54c134195489d7d52
Aiello Chan <Aiello.Chan@Gmail.com>
2018-04-12 20:10:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="app">
+    <div class="container title">
+      <a href="">Vue-Tags</a>
+    </div>
+    <div class="container vue-tags">
+      <Tags :AllTags="allTags"
+        :UsedTagIds="usedTagIds"
+        AutoComplite='true'></Tags>
+    </div>
+    <div class="container info">
+      <h3>食用指导</h3>
+      <small>通过一个简单的 5 步教学，让你了解本控件所有功能！</small>
+      <h4>1. 添加</h4>
+      <p>尝试着在输入框中输入
+        <mark>Google</mark>，然后按
+        <strong>Enter</strong> 键，即可添加该标签！</p>
+      <h4>2. 删除标签</h4>
+      <p>那个名为
+        <mark>Mini</mark> 的标签我也不知道是干嘛的，通过点击该标签右侧的
+        <red>✕</red> 来将其移除。 </p>
+      <p>当前我们的输入框什么都没有，请在输入框中点击一下，让它获得焦点。然后我觉得这个
+        <mark>Google</mark> 也没什么用，按下
+        <strong>Backspace</strong> 来删除最后一个元素。</p>
+      <h4>3. 自动补全</h4>
+      <p>嗯，我觉得应该加上
+        <mark>Kubernetes</mark> 这个标签，让我们先输入
+        <mark>Ku</mark> 这两个字符，这个单词太长了，我不想挨个儿输完(其实是忘了怎么拼写😂)。<br/> 那可以偷个懒，看到了输入框中那个浅蓝色的文字吗？插件已经知道我们要输入什么了，你只需要按下
+        <strong>Tab</strong> 键即可瞬间自动补全（也可以鼠标双击输入框），补全后再按
+        <mark>Enter</mark> 将其添加进去！
+      </p>
+      <h4>4. 重复提示</h4>
+      <p>好像不对，刚才添加进去的
+        <mark>kubernetes</mark> 怎么是小写的！我想要这种效果的：
+        <mark>Kubernetes</mark>，那试着再在输入框中输入
+        <mark>Ku</mark> 然后自动补全。完了，怎么自动补全直接就把我刚才的大写
+        <mark>K</mark> 换成小写了。。。好吧，手动修改第一个字母为大写！完美！然后按
+        <strong>Enter</strong>。</p>
+      <p>Naive！控件检测到这两个标签除了大小写不同其他都一样，所以拒绝了添加这个控件，并且通过闪动已有标签的方式提醒你：这个标签加过啦！</p>
+      <h4>5. 修改大小写</h4>
+      <p>如果只是大小写不同，控件是会拒绝添加的（因为本来就是一个东西），如果想要修改已有标签的大小写，有一个非常简单的办法：同时按下
+        <strong>Shift</strong> 和
+        <strong>Enter</strong>，完事儿！</p>
+      <p></p>
+      <p>这些便是基础用法，更多功能只有你自己去发现了😼</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tags from "./components/Tags";
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      allTags: {
+        1: { name: 'Github', color: '#213123' },
+        2: { name: 'JavaScript', color: '#213123' },
+        3: { name: 'Golang', color: '#213123' },
+        4: { name: 'Blog', color: '#213123' },
+        5: { name: 'Mini', color: '#213123' },
+        6: { name: 'MacOS', color: '#213123' },
+        7: { name: 'Node.js', color: '#213123' },
+        8: { name: 'kubernetes', color: '#213123' }
+      },
+      usedTagIds: [3, 5, 7],
+    }
+  },
+  components: {
+    Tags
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  padding: 60px 0 0;
+  width: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.container {
+  box-sizing: border-box;
+  margin: auto;
+  width: 400px;
+  &.title {
+    margin-bottom: 50px;
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    a,
+    a:hover,
+    a:link,
+    a:active,
+    a:visited {
+      cursor: pointer;
+      text-decoration: none;
+      color: #24292e;
+    }
+  }
+  &.vue-tags {
+    background: #eee;
+  }
+  &.info {
+    margin-top: 10px;
+    padding: 5px;
+    background: #eee;
+    p {
+      font-size: 0.8rem;
+    }
+    red {
+      color: red;
+    }
+  }
+}
+</style>
---
+++ src/components/Tags.vue
@@ -0,0 +1,567 @@
+<template>
+  <div class="vue-tags-container">
+    <div class="used-tags-wapper">
+      <div v-for="tagId in usedTagIds"
+        :key="tagId"
+        :class="{active:hlTagId==tagId}"
+        class="used-tag">
+        <span>{{getTagNameById(tagId)}}</span>
+        <span @click="delTag(tagId)"
+          class="used-tag-remove">✕</span>
+      </div>
+      <span class="new-tag-wapper">
+        <span class="new-tag-item new-tag-propose">{{proposal}}</span>
+        <input v-model="inputVal"
+          :placeholder="Placeholder"
+          :class="{'no-select':proposedTagId!==null,warning:showInputWarning}"
+          @dblclick.prevent="onKeydownTab"
+          @keypress.enter="onKeypressEnter"
+          @keydown.delete="onKeydownDelete"
+          @keyup.delete="onKeyupDelete"
+          @compositionstart="onCompositionStart"
+          @compositionend="onCompositionEnd"
+          @paste.prevent="onPaste"
+          @keydown.9.prevent="onKeydownTab"
+          type="text"
+          class="new-tag-item new-tag-input">
+      </span>
+    </div>
+    <div calss="unused-tags-wapper"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Vue-Tags",
+  props: {
+    /* Datas */
+    UsedTagIds: {
+      describ: '正在使用的标签所对应的 id 数组，如: [2, 3, 6]',
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    AllTags: {
+      describ: '所有的标签项目数组，如: {1: {name: "Github", color: "#213123"}}',
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    NextTagId: {
+      describ: '全局 id 指示器，新增的标签 id 就会使用这个值并且将其自增 1',
+      type: Number,
+      default: null
+    },
+    Placeholder: {
+      describ: '输入框的占位提示文字',
+      type: String,
+      default: '请输入标签名'
+    },
+    AutoComplite: {
+      describ: '在输入框中自动补全，会多消耗一丢丢性能',
+      type: Boolean | String,
+      default: false
+    },
+    AllowPasteToAdd: {
+      describ: '允许通过粘贴直接添加标签',
+      type: Boolean | String,
+      default: true
+    },
+
+    /* Events */
+    /* 
+    * 所有钩子函数都会收到一个对象作为参数 
+    * 
+    * {
+    *   allTags:{}
+    *   usedTagIds:[]
+    *   globalNextId:10
+    * }
+    */
+    BeforeAddTag: {
+      describ: '在添加标签前的钩子函数，将会返回即将被加入的标签',
+      type: Function,
+      default: function () { }
+    },
+    AfterAddTag: {
+      describ: '在添加标签后的钩子函数，将会返回所有的标签',
+      type: Function,
+      default: function () { }
+    },
+    BeforeUseTag: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+    AfterUseTag: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+    BeforeDeleteTag: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+    AfterDeleteTag: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+    BeforeUpdateTagName: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+    AfterUpdateTagName: {
+      describ: '',
+      type: Function,
+      default: function () { }
+    },
+
+    /* Styles */
+    WarningLength: {
+      describ: '标签输入框中输入超过设定值数量的字符，输入字体颜色将改变',
+      type: Number,
+      default: 18
+    }
+  },
+  mounted() {
+    // 初始化全局 id
+    this.globalNextId = this.initGlobalId();
+  },
+  data() {
+    return {
+      /* Data */
+      allTags: this.AllTags, // 所有的标签
+      usedTagIds: this.UsedTagIds, // 使用中的标签的 id
+      globalNextId: 10, // 全局 id 指示器
+      inputVal: '', // 新标签输入框的值
+      proposedTagId: null, // 输入框中自动补全所对应标签的 id
+      /* State */
+      showInputWarning: false,
+      isComposing: false, // 用于处理：使用中文输入法时点击删除会删除标签
+      hlTagId: null, // 高亮闪烁的标签 id，一般用于提醒该标签已在使用中
+      hlCount: 0, // 用以高亮计数，避免连续的高亮不独立计时
+      delKeySwitch: true, // 防止按住 delete 键后连续删除标签
+    };
+  },
+  computed: {
+    // 输入框中的建议值（已处理了大小写问题）
+    proposal() {
+      // 匹配到的已有标签的完整名称
+      let name = this.proposedTagName,
+        val = this.inputVal;
+
+      // 没有匹配到的情况
+      if (name === '') return '';
+
+      // 下面一顿操作是为了让不同大小写的文字能对应（比如）
+      // TODO: 详细解释代码作用
+      name = name.substr(val.length);
+      name = val + name;
+      return name;
+    },
+    // 当前输入框匹配标签的完整名字，未经过大小写转换的
+    proposedTagName() {
+      // 匹配成功
+      if (this.proposedTagId !== null) {
+        return this.getTagNameById(this.proposedTagId);
+      }
+      return '';
+    },
+    // 回调函数的参数
+    cbParam() {
+      return {
+        allTags: this.allTags,
+        usedTagIds: this.usedTagIds,
+        globalNextId: this.globalNextId
+      }
+    }
+  },
+  methods: {
+    /*
+     * 初始化函数 
+     * */
+    // 初始化全局 id 计数器
+    initGlobalId() {
+      let maxId = 0;
+      // 如果 allTags 不为空，则计算出其中最大的 id 值
+      let ids = Object.keys(this.allTags);
+      if (ids.length !== 0) {
+        maxId = Math.max(...ids) + 1;
+      }
+      // 如果用户提供了指定的 id 值，则进行处理
+      if (this.NextTagId !== null) {
+        maxId = maxId > this.NextTagId ? maxId : this.NextTagId;
+      }
+      // 用户未提供
+      return Number(maxId);
+    },
+
+    /*
+     * 事件处理 
+     * */
+    onKeypressEnter(event) {
+      let newTagName = this.inputVal,
+        isSuccess = false;
+      // 值为空，不予处理
+      if (newTagName === '') return;
+
+      // 强制替换模式
+      if (event.shiftKey) {
+        isSuccess = this.addTag(this.inputVal, true);
+      } else {
+        // 普通增加模式
+        isSuccess = this.addTag(this.inputVal);
+      }
+
+      // 添加成功，将输入框置空
+      if (isSuccess) {
+        this.hlTagId = null;
+        this.inputVal = '';
+      }
+    },
+    onKeydownDelete() {
+      // 正在使用弹出式输入框输入（如中文输入法），则跳过处理 delete 事件
+      if (this.isComposing) {
+        this.delKeySwitch = false;
+        return;
+      }
+
+      // 输入框值不为空，则不能删除前面的标签
+      if (this.inputVal.length !== 0) {
+        this.delKeySwitch = false;
+        return;
+      }
+
+      // 没有删除机会，无法删除标签
+      // 只要有 delete 操作，就不允许直接删除，必须先抬起 delete 键一次才能获得一次删除机会
+      if (this.delKeySwitch) {
+        this.delLastTag();
+        this.delKeySwitch = false;
+      }
+    },
+    onKeyupDelete() {
+      this.delKeySwitch = true;
+    },
+    onCompositionStart() {
+      this.isComposing = true;
+    },
+    onCompositionEnd() {
+      this.isComposing = false;
+    },
+    // 屏蔽浏览器默认操作
+    onPaste(e) {
+      // 获取剪贴板数据
+      let text = (e.clipboardData || window.clipboardData).getData('text');
+      // 判断是否开启粘贴添加的功能并且数据不能为空
+      if (this.AllowPasteToAdd && text.length > 0) {
+        if (this.addTag(text)) {
+          // 添加成功后将输入框置空
+          this.inputVal = '';
+        } else {
+          // 添加失败后，将数据填写在输入框，供修改
+          this.inputVal = text;
+        }
+      }
+    },
+    onKeydownTab() {
+      if (this.AutoComplite) {
+        // 避免在没有匹配项的情况下替换当前输入
+        if (this.proposedTagId === null) return;
+
+        this.inputVal = this.proposedTagName;
+      }
+    },
+
+    /* 
+     * 功能函数 
+     * */
+    // 添加
+    addTag(name, force) {
+      // 去除前后空格，其实只需要去除后面的空格，但为了完全覆盖，这一点性能损耗不算什么
+      name = name.trim();
+      // 值为空不予以处理
+      if (name.length === 0) return false;
+      // 检查是否已存在此标签
+      let existedTagId = this.getTagIdByName(name, false);
+      if (existedTagId === null) {
+        // 并没有添加过这个名称的标签
+        // 构建新标签
+        let newTag = new Object({
+          name: name
+        }),
+          newTagId = this.getNextId();
+        // 添加到标签列表中
+        this.allTags[newTagId] = newTag;
+        this.useTag(newTagId);
+      } else if (this.usedTagIds.indexOf(existedTagId) === -1) {
+        // 已经存在此名字的标签，但是当前并没有使用
+        // 使用该标签
+        this.useTag(existedTagId);
+      } else {
+        // 强制添加
+        if (force) {
+          // 则先将已有标签的名字改掉
+          this.setTagNameById(existedTagId, name);
+          return true;
+        } else {
+          // 该名称对应标签正在被使用
+          this.hlTagById(existedTagId);
+          // 添加失败，返回 false
+          return false;
+        }
+      }
+
+      return true;
+    },
+    useTag(id) {
+      // 直接添加到当前使用列表中
+      this.usedTagIds.push(id);
+    },
+    // 移除指定标签
+    delTag(id) {
+      this.usedTagIds = this.rmMatch(this.usedTagIds, _id => _id !== id);
+    },
+    // 从使用中的标签里移除最后一个
+    delLastTag() {
+      this.usedTagIds = this.rmLast(this.usedTagIds);
+    },
+    inputWarning() {
+      // 输入超过指定长度给予提醒
+      if (this.inputVal.length > this.WarningLength) {
+        this.showInputWarning = true;
+      } else {
+        this.showInputWarning = false;
+      }
+    },
+    inputPropose() {
+      if (this.AutoComplite) {
+        let val = this.inputVal,
+          matchedTagId = null; // 模糊匹配到的已有标签 id
+        // 无任何输入的情况下不进行建议
+        if (val.length !== 0) {
+          // 查询数据并给出首次匹配的标签 id
+          matchedTagId = this.getTagIdByName(val, false, true, true);
+        }
+        this.proposedTagId = matchedTagId;
+      }
+    },
+    getNextId() {
+      return this.globalNextId++;
+    },
+    getTagNameById(id) {
+      return this.allTags[id].name;
+    },
+    // isAbsLike 表示绝对模糊匹配，匹配的只能相似，不能相同
+    // 成功返回 id Number，失败返回 null
+    getTagIdByName(name, isCase, isLike, isAbsLike) {
+      let allTags = this.allTags,
+        id = null;
+      // 初始化默认值
+      isCase = isCase ? true : false;
+      isLike = isLike || false;
+      isAbsLike = isAbsLike || false;
+
+      if (!name) return;
+      // 大小写不敏感则全转化为小写来比较
+      if (!isCase) name = name.toLowerCase();
+
+      Object.keys(allTags).find(key => {
+        let curName = allTags[key].name;
+        if (!isCase) curName = curName.toLowerCase();
+        // 简单模糊匹配
+        if (isLike) {
+          if (curName.startsWith(name)) {
+            // 绝对模糊匹配
+            if (isAbsLike) {
+              if (curName.length === name.length) {
+                // 两者完全相同，不符合绝对模糊匹配要求
+                return false;
+              } else {
+                // 符合绝对匹配要求，匹配成功
+                id = key;
+                return true;
+              }
+            }
+            // 非绝对模糊匹配则直接返回即可
+            id = key;
+            return true;
+          }
+        } else {
+          if (curName === name) {
+            id = key;
+            return true;
+          }
+        }
+      });
+      return id === null ? null : Number(id);
+    },
+    // 通过 id 来直接修改对应标签的显示名字,用于更新大小写
+    setTagNameById(id, name) {
+      if (this.allTags[id] && name.length > 0) {
+        this.allTags[id].name = name;
+        return true;
+      }
+      return false;
+    },
+    // highlight 标签
+    hlTagById(id) {
+      this.hlTagId = id;
+      this.hlCount++;
+
+      setTimeout(() => {
+        this.hlCount--;
+        if (this.hlCount === 0) {
+          this.hlTagId = null;
+        }
+      }, 1600);
+    },
+
+    /* 
+     * 辅助函数 
+     * */
+    rmLast(arr) {
+      if (arr.length === 0) {
+        return arr;
+      }
+      if (arr.length === 1) {
+        return [];
+      }
+      arr.splice(-1);
+      return arr;
+    },
+    rmMatch(arr, func) {
+      return arr.filter(func);
+    },
+    // 去除左侧所有空格、换行、制表符
+    triml(str) {
+      return str.replace(/^[\s\n\t]+/g, "");
+    },
+    // FIXME: 暂未使用的函数
+    trimr(str) {
+      return str.replace(/[\s\n\t]+$/g, "");
+    },
+    getStrRealLength(str) {
+      return str.replace(/[\u0391-\uFFE5]/g, "aa").length;  //先把中文替换成两个字节的英文，在计算长度
+    },
+    // FIXME: 暂未使用的函数
+    // 获得随机数，n 为长度
+    getRndNum(n) {
+      let rnd = '';
+      for (var i = 0; i < n; i++)
+        rnd += Math.floor(Math.random() * 10);
+      return rnd;
+    },
+  },
+  watch: {
+    inputVal(val) {
+      // 文字过长警告
+      this.inputWarning();
+      // 自动补全
+      this.inputPropose();
+      // 禁止全为空格的数据
+      if (val[0] === ' ') {
+        this.inputVal = this.triml(this.inputVal);
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$c-light: #fff;
+$c-info: #a2ceff;
+$c-orange: #e36209;
+$c-warning: #d73a49;
+$c-secondary: #787878;
+$c-dark: #24292e;
+
+$fs-sm: 0.6rem;
+$fs-s: 0.8rem;
+$fs-m: 1rem;
+
+$input-width: 7rem;
+
+.vue-tags-container {
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 2px;
+  font-size: $fs-m;
+}
+.used-tags-wapper {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+.used-tag {
+  margin: 2px;
+  padding: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #24292e;
+  border-radius: 3px;
+  color: $c-light;
+  font-size: $fs-m;
+  &.active {
+    animation: blink 0.4s linear infinite alternate;
+  }
+}
+.new-tag-wapper {
+  position: relative;
+  flex-grow: 1;
+  margin: 0;
+  padding: 0;
+  width: $input-width;
+}
+.new-tag-item {
+  box-sizing: border-box;
+  margin: 4px 2px 4px 4px;
+  border: none;
+  padding: 2px 0;
+  width: calc(100% - 6px);
+  font-size: $fs-m;
+}
+.new-tag-propose {
+  position: absolute;
+  color: $c-info;
+}
+.new-tag-input {
+  position: inherit;
+  background: none;
+  outline: none;
+  border-radius: 2px;
+  font: inherit;
+  transition: 300ms color;
+  &.warning {
+    color: $c-orange;
+  }
+  &.no-select::-moz-selection {
+    background: transparent;
+  }
+  &.no-select::selection {
+    background: transparent;
+  }
+}
+.used-tag-remove {
+  margin-left: 4px;
+  cursor: pointer;
+  font-size: $fs-s;
+  &:hover {
+    color: $c-warning;
+  }
+}
+@keyframes blink {
+  to {
+    color: #24292e;
+    background: #c3c3c3;
+  }
+}
+</style>
